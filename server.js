ca6271dbd36529de6a85c8e914d04416032485ac
Chris Natoli <chrisnatoli@gmail.com>
2021-02-15 12:36:50 -0500

    Replace clock and random number every minute with just a button that generates a new random number

--- server.js
+++ server.js
@@ -7,29 +7,44 @@ const INDEX = '/index.html';
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
 const wss = new Server({ server });
+
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
+
+  ws.on('message', (incomingMsg) => {
+    console.log('Received a message')
+
+    const num = Math.floor(Math.random() * 1000);
+    const outgoingMsg = {
+      num: num
+    };
+    wss.clients.forEach((client) => client.send(JSON.stringify(outgoingMsg)));
+    console.log(`Sent ${num}`)
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
 });
 
-
-let random_int = Math.floor(Math.random() * 1000);
+/*
+let num = Math.floor(Math.random() * 1000);
 
 setInterval(() => {
   wss.clients.forEach((client) => {
     time = new Date();
     if (time.getSeconds() === 0) {
-      random_int = Math.floor(Math.random() * 1000)
+      num = Math.floor(Math.random() * 1000)
     }
 
     const message = {
       time: time.toTimeString(),
-      int: random_int
+      num: num
     };
-    
+
     client.send(JSON.stringify(message));
   });
 }, 1000);
+*/

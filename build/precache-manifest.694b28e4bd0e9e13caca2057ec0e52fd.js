5754bb4153e91ecd5088ee1185d755b29909fa02
Chris Natoli <chrisnatoli@gmail.com>
2021-02-23 18:53:16 -0500

    Run build

---
+++ build/precache-manifest.694b28e4bd0e9e13caca2057ec0e52fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a453c28d739b0d6cd7d0",
+    "url": "/static/css/main.eb55074d.chunk.css"
+  },
+  {
+    "revision": "a453c28d739b0d6cd7d0",
+    "url": "/static/js/main.a453c28d.chunk.js"
+  },
+  {
+    "revision": "31783d9933936ff5290c",
+    "url": "/static/js/1.31783d99.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2755cd3d7adc82d9dca5c12ba0f0e506",
+    "url": "/index.html"
+  }
+];

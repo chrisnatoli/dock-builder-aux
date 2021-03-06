92785a27db9182af38c8c45b98b4cea7186da565
Chris Natoli <chrisnatoli@gmail.com>
2021-03-06 11:05:59 -0500

    Shuffle horizon deck during initialization

--- src/game_state/Deck.js
+++ src/game_state/Deck.js
@@ -1,11 +1,25 @@
 const initHorizonDeck = () => {
   const nums = [...Array(10).keys()];
-  const drawPile = nums.map(i => ({
+  let drawPile = nums.map(i => ({
     id: "card"+i,
     num: i,
   }));
+  drawPile = shuffle(drawPile);
   const discardPile = [];
   return { drawPile, discardPile };
+}
+
+const shuffle = (deck) => {
+  let newDeck = [...deck];
+  let n = deck.length;
+
+  while (n) {
+    const i = Math.floor(Math.random() * n);
+    n--;
+    [newDeck[i], newDeck[n]] = [newDeck[n], newDeck[i]];
+  }
+
+  return newDeck;
 }
 
 const drawCard = (drawPile, hand) => {

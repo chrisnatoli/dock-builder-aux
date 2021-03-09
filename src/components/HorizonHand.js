adbeb4d502d9412b1976d7e0fc085479a082db3f
Chris Natoli <chrisnatoli@gmail.com>
2021-03-09 10:56:37 -0500

    Make the player's hand of cards a form with radio buttons, one per card

--- src/components/HorizonHand.js
+++ src/components/HorizonHand.js
@@ -2,14 +2,40 @@ import React from 'react';
 import HorizonCard from './HorizonCard';
 
 class HorizonHand extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { selectedOption: null };
+  }
+
+  handleChange = (event) => {
+    this.setState({ selectedOption: event.target.value });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
   render() {
     const { hand } = this.props;
+    const { selectedOption } = this.state;
 
     return (
       <div className="HorizonHand">
-        {hand.map(card => (
-          <HorizonCard card={card} key={card.id} />
-        ))}
+          <form onSubmit={this.handleSubmit}>
+            {hand.map(card => (
+              <label>
+                <input
+                  type="radio"
+                  value={card.id}
+                  checked={selectedOption===card.id}
+                  onChange={this.handleChange}
+                  />
+                <HorizonCard card={card} key={card.id} />
+              </label>
+            ))}
+
+            <input type="submit" value="Submit" />
+          </form>
       </div>
     );
   }
